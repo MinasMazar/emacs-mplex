e39cf6b13289131d96a9e94bb675564bf63563eb
MinasMazar <minasmazar@gmail.com>
2016-12-27 04:02:12 +0100

    Initial commit: bundle gem emacs-mplex

---
+++ lib/emacs/mplex.rb
@@ -0,0 +1,7 @@
+require "emacs/mplex/version"
+
+module Emacs
+  module Mplex
+    # Your code goes here...
+  end
+end
---
+++ lib/emacs/mplex/version.rb
@@ -0,0 +1,5 @@
+module Emacs
+  module Mplex
+    VERSION = "0.1.0"
+  end
+end
